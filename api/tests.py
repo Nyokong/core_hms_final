077158e8a0703472314f0d23eb76277518467c21
Busi-Blessing <busisiwematsoele7@gmail.com>
2024-09-20 08:10:57 +0200

    tests the views

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+
+from django.urls import reverse
 from django.core.exceptions import ValidationError
 from .models import custUser, Lecturer, Student, FeedbackMessage
 
-# Create your tests here.
+# Model Tests
 class CustUserModelTest(TestCase):
 
     def setUp(self):
@@ -42,4 +44,30 @@ class FeedbackMessageModelTest(TestCase):
         user = custUser.objects.create_user(username='testuser', password='testpass')
         message = FeedbackMessage.objects.create(user=user, message="This is a test message.")
         self.assertEqual(message.user.username, 'testuser')
+        
         self.assertEqual(message.message, "This is a test message")
+
+#View Test 
+
+class MyViewTest(TestCase):
+    def test_my_view(self):
+        response = self.client.get(reverse('my-view'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'message': 'token works'})
+
+class FeedbackMessageViewTest(TestCase):
+    def setUp(self):
+        self.user = custUser.objects.create_user(username='testuser', password='testpass')
+        self.feedback_message = FeedbackMessage.objects.create(user=self.user, message="This is a test message.")
+
+    def test_get_feedback_messages(self):
+        response = self.client.get(reverse('feedback-msgs'))  # Update with the actual URL name
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)  # Assuming one message exists
+        self.assertEqual(response.data[0]['message'], "This is a test message.")
+
+    def test_get_feedback_messages_empty(self):
+        FeedbackMessage.objects.all().delete()  # Clear all feedback messages
+        response = self.client.get(reverse('feedback-msgs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [])  # Expect an empty list
