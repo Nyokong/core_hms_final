1fcf434eeca0a987e2ae902e49b9bfa5493cfeaf
Nyokong <50559078+Nyokong@users.noreply.github.com>
2024-09-27 21:54:48 +0200

    last last last commit

--- core/settings.py
+++ core/settings.py
@@ -216,12 +216,12 @@ ASGI_APPLICATION = "core.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 
@@ -234,16 +234,16 @@ AZURE_CONTAINER=os.getenv("AZURE_CONTAINER")
 #     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER"),
-#         'PASSWORD': os.getenv("DB_PASSWORD"),
-#         'HOST': 'db',  # This should match the service name in docker-compose.yml
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),  # This should match the service name in docker-compose.yml
+        'PORT': os.getenv("DB_PORT"),
+    }
+}
 
 CELERY_IMPORTS = ('api.tasks',)
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
