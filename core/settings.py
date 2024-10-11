29a079bba1084663a33428413a517c5474c2e924
Progress Nemaranzhe <140755656+KP-Nemaranzhe@users.noreply.github.com>
2024-10-11 14:47:04 +0200

    settings

--- core/settings.py
+++ core/settings.py
@@ -109,6 +109,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework_simplejwt',
+    'rest_framework.authtoken'
 
     # install app api
     'api',
